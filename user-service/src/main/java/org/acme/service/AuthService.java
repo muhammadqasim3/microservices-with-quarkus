dce8dc78b75b2ba46b8525e7ed2437631dfc7563
m.qasimaslam <muhammadqasimaslam3@gmail.com>
2025-06-02 18:19:17 +0500

    MQ: JWT Authentication

---
+++ user-service/src/main/java/org/acme/service/AuthService.java
@@ -0,0 +1,20 @@
+package org.acme.service;
+
+
+import io.smallrye.jwt.build.Jwt;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class AuthService {
+
+    public String generateJwt() {
+        long nowSecs = System.currentTimeMillis() / 1000;
+        long expSecs = nowSecs + 3600;
+
+        return Jwt.issuer("auth-service")
+                .subject("auth-service")
+                .groups("Admin")
+                .expiresAt(expSecs)
+                .sign();
+    }
+}
